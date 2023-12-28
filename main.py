e3342036c24b763e37a21fba0a23c2107be054fe
KhizarBajwa <48625072+KhizarBajwa@users.noreply.github.com>
2023-12-28 13:40:08 -0500

    added print_commands method to main.py to show all possible commands

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # main.py
-from database import Database
 
+from database import Database
+from colorama import Fore, Style, init
+
+# Initialize colorama for colored output
+init(autoreset=True)
+
+def print_commands():
+    print(f"\n{Fore.BLUE}Available Commands:{Style.RESET_ALL}")
+    print(f"1. {Fore.CYAN}Fetch all users{Style.RESET_ALL}")
+    print(f"2. {Fore.CYAN}Insert a new user{Style.RESET_ALL}")
+    print(f"3. {Fore.CYAN}Update user email{Style.RESET_ALL}")
+    print(f"4. {Fore.CYAN}Delete a user{Style.RESET_ALL}")
+    print(f"5. {Fore.CYAN}Fetch orders by user{Style.RESET_ALL}")
+    print(f"6. {Fore.RED}Execute custom query{Style.RESET_ALL}")
+    print(f"7. {Fore.RED}Exit{Style.RESET_ALL}")
+
+
+# Instantiate the Database class
 db = Database("app.db")
 
-# Create tables or perform other database initialization steps
-
-while True:
-    user_input = input("Enter a command: ")
-
-    if user_input.lower() == "select_all_users":
-        users = db.select_all_users()
-        for user in users:
-            print(user)
-
-    # Add other command handling as needed
-
-    elif user_input.lower() == "exit":
-        break
+print_commands()
